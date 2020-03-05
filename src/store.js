3da624aceaa828f4943c0cbc34159c298af59428
superliner <ankit.mohan@live.in>
2020-03-05 19:03:00 +0530

    Implemented notes frontend

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "./features/todo/todoSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    todos: todosReducer
+  }
 });
